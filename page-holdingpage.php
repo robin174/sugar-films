20b1389c4cd236a523a48e4385887696632ad544
Robin Green <robin@designingreen.co.uk>
2020-06-30 12:22:11 +0100

    holding page

---
+++ page-holdingpage.php
@@ -0,0 +1,34 @@
+<?php 
+/*
+Template Name: Page - Holding Page
+*/
+get_header(); ?>
+	
+	<div class="content">
+		<div class="inner-content grid-x">
+	
+		    <main class="main small-12 medium-12 large-12 cell" role="main">
+
+		    	<div class="grid-container">
+			    	<div class="grid-x grid-padding-x">
+			    		<div class="small-12 medium-12 large-12 cell">
+			    			<section>
+			    				<figure>
+			    					<img src="">
+			    				</figure>
+			    			</section>
+			    			<section style="padding-top: 100px;padding-bottom:100px;">
+								<h1>Welcome to Sugar Films.</h1>
+								<p>We are currently updating our website and will be live soon.</p>
+								<p style="margin-bottom:0;">To contact the team, please email <a href="mailto:contact@sugarfilms.co.uk" title="Email Sugar Films">contact@sugarfilms.co.uk</a>.</p>
+			    			</section>
+			    		</div>
+			    	</div>
+			    </div>
+			    					
+			</main> <!-- end #main -->
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
