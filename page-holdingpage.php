c66b14e8174dc1e804b13c1d10799a12b05b491a
Robin Green <robin@designingreen.co.uk>
2020-09-22 18:23:01 +0100

    include logo

--- page-holdingpage.php
+++ page-holdingpage.php
@@ -12,12 +12,12 @@ get_header(); ?>
 		    	<div class="grid-container">
 			    	<div class="grid-x grid-padding-x">
 			    		<div class="small-12 medium-12 large-12 cell">
-			    			<section>
+			    			<section style="padding-top: 75px;">
 			    				<figure>
-			    					<img src="">
+			    					<img src="<?php echo get_template_directory_uri(); ?>/images/sugarfilms-logo-sm.png" title="Sugar Films">
 			    				</figure>
 			    			</section>
-			    			<section style="padding-top: 100px;padding-bottom:100px;">
+			    			<section style="padding-top: 50px;padding-bottom:100px;">
 								<h1>Welcome to Sugar Films.</h1>
 								<p>We are currently updating our website and will be live soon.</p>
 								<p style="margin-bottom:0;">To contact the team, please email <a href="mailto:contact@sugarfilms.co.uk" title="Email Sugar Films">contact@sugarfilms.co.uk</a>.</p>
